a410a2cef49427ce8d22a133158881d94bcd0839
Leonardo Monteiro <leomonteiro.outsmart@gmail.com>
2021-07-04 06:34:14 -0300

    remove unused apollo error

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,6 +1,5 @@
 import { Inject } from '@nestjs/common'
 import { Args, Query, Resolver } from '@nestjs/graphql'
-import { ApolloError } from 'apollo-server-errors'
 import { UserGetInfoGateway, USER_GETINFO } from 'src/core/user'
 import { UserType } from './user.type'
 
@@ -13,11 +12,6 @@ export class UserResolver {
 
   @Query(() => UserType)
   getInfo(@Args('email') email: string): Promise<UserType> {
-    try {
-      return this.userGetInfo.execute(email)
-    } catch (err) {
-      console.log(err)
-      throw new ApolloError(err.message)
-    }
+    return this.userGetInfo.execute(email)
   }
 }
